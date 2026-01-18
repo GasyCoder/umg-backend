6f03ad81d31056a4d76bdcc04e244a84ac30ea03
GasyCoder <bezaraflorent@gmail.com>
2026-01-18 22:06:42 +0300

    Return JSON validation errors for contact API

--- app/Http/Requests/Public/ContactMessageRequest.php
+++ app/Http/Requests/Public/ContactMessageRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Public;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ContactMessageRequest extends FormRequest
 {
@@ -14,7 +16,16 @@ class ContactMessageRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255'],
             'subject' => ['required', 'string', 'max:255'],
-            'message' => ['required', 'string', 'min:10', 'max:200'],
+            'message' => ['required', 'string', 'min:10', 'max:2000'],
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Always return JSON for API calls, even if the client didn't send Accept: application/json.
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation error.',
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
